6dc98677e2ee14b864b7f3f93eab558ccb0c4413
Carol Nichols <cnichols@thinkthroughmath.com>
2014-09-12 16:38:36 -0400

    Scoring is working!
    No tie breaking though.

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,6 +3,18 @@ class QuizController < ApplicationController
   end
 
   def score
+    results = Hash.new { 0 }
+    params.each do |key, value|
+      if answers = QUIZ_DATA[key]
+        if similar_results = answers[value]
+          similar_results.each do |result|
+            results[result] += 1
+          end
+        end
+      end
+    end
+
+    @top_result = results.max_by { |k, v| v }.first
     render :result
   end
 end
